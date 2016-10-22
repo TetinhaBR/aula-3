29a4b3affb0156b9dd4d0ca24ac1fa598ff86f52
Guilherme Cordeiro <Guilherme Cordeiro>
2016-10-22 15:05:40 -0200

    aula - 22/10/2016

--- index.php
+++ index.php
@@ -9,8 +9,27 @@ require_once 'vendor/autoload.php';
     <head>
         <meta charset="utf-8">
         <title>Meu Curso Legal</title>
+        <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/bulma/0.2.3/css/bulma.min.css"
+         integrity="sha256-F7gqKszCwmz8vhiti+AICU8dLfIEpxzPVihhhGfbbKg=" crossorigin="anonymous" />
     </head>
     <body>
+        <nav class="nav has-shadow">
+          <div class="container">
+
+            <div class="nav-left">
+                <a class="nav-item" href = "index.php">
+                  <img src="http://www.fagoc.br/download/a/logo" alt="logo"/>
+                </a>
+            </div>
+
+            <div class="nav-right nav-menu">
+                <a class="nav-item" href="index.php?e-6"> Exercicio 6 </a>
+                <a class="nav-item" href="index.php?e-7"> Exercicio 7 </a>
+                <a class="nav-item" href="index.php?e-8"> Exercicio 8 </a>
+            </div>
+
+          </div>
+        </nav>
         <?php
         $exercicio = __DIR__ . '/exercicio-' . get('e') . '/' . 'file.php';
         if (file_exists($exercicio)) {
